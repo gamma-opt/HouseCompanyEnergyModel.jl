ef8aabbc370bda322debc5d190ed611708929f3b
Helmi Hankimaa <helmi.hankimaa@gmail.com>
2022-08-08 16:28:09 +0300

    Included constraint testing in testing.

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,6 +5,6 @@ using PredicerTestVersion
 @testset "PredicerTestVersion" begin
     include("structures.jl")
     include("variables.jl")
-    #include("constraints.jl")
+    include("constraints.jl")
     include("objective.jl")
 end
